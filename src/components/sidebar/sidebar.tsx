07235463b5d5d84fdf74cbab3d1565900fbf19d9
PeiYun <ollylee8520@gmail.com>
2024-04-10 22:27:46 -0600

    add profile comp

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,5 +1,6 @@
 import React, { FC, ReactElement } from "react";
 import { Grid } from "@mui/material";
+import { Profile } from "../profile/profile";
 
 export const Sidebar: FC = (): ReactElement => {
   return (
@@ -19,7 +20,7 @@ export const Sidebar: FC = (): ReactElement => {
         alignItems: "center",
       }}
     >
-      <h2>Sidebar Area</h2>
+      <Profile />
     </Grid>
   );
 };
