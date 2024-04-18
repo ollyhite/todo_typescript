410d11b1bb57fb06b5a133519ff55c55bfdd73d2
PeiYun <ollylee8520@gmail.com>
2024-04-17 22:08:23 -0600

    try to fix QueryClientProvider error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import React, { FC, ReactElement } from "react";
+import React, {
+  FC,
+  ReactElement,
+  useEffect,
+  useState,
+  Suspense,
+  lazy,
+} from "react";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { customTheme } from "./theme/customTheme";
 import { Dashboard } from "./pages/dashboard/dashboard";
@@ -7,10 +14,30 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ComposeContext from "./context/Compose.context";
 import { rootContext } from "./context/root.context";
 
-//create a client
-const queryClient = new QueryClient();
-
 const App: FC = (): ReactElement => {
+  const ReactQueryDevtoolsProduction = lazy(() =>
+    import("@tanstack/react-query-devtools/build/modern/production.js").then(
+      (d) => ({
+        default: d.ReactQueryDevtools,
+      })
+    )
+  );
+  //create a client
+  // const queryClient = new QueryClient();
+  const [showDevtools, setShowDevtools] = useState(false);
+
+  useEffect(() => {
+    // @ts-ignore
+    window.toggleDevtools = () => setShowDevtools((old) => !old);
+  }, []);
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 5, // added the staleTime for queries globally
+      },
+    },
+  });
   return (
     <QueryClientProvider client={queryClient}>
       <ComposeContext components={rootContext}>
@@ -20,6 +47,11 @@ const App: FC = (): ReactElement => {
         </ThemeProvider>
       </ComposeContext>
       <ReactQueryDevtools initialIsOpen={false} />
+      {showDevtools && (
+        <Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </Suspense>
+      )}
     </QueryClientProvider>
   );
 };
